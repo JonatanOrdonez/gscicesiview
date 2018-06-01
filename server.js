d2e744546c3673903f96ab1cf3b5050014ff0022
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-06-01 06:05:45 -0500

    update server

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
