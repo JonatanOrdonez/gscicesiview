0c7b51f801e6531d3969de5ae2b3356894644aab
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-06-01 07:46:53 -0500

    algo

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
-});
-app.get('/admin', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
-});
+app.listen(process.env.PORT || 8080);
+app.use('/', express.static('public'));
+
+app.get('/admin', function (req, res) {
+    res.send('admin');
+  });
+  
+app.get('/home', function (req, res) {
+    res.send('home');
+  });
+  
