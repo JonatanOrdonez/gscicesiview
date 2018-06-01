c922934761927f60cafa9299164c57ea5c9c4617
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-06-01 07:53:07 -0500

    set status

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.use('/', express.static('public'));
 
-app.get('/admin', function (req, res) {
-    res.send('admin');
-  });
-  
-app.get('/home', function (req, res) {
-    res.send('home');
-  });
+app.use('/home', express.static('public'));
+
+app.use('/admin', express.static('public'));
   
